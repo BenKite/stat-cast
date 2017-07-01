3629c8b12a2e06049a6d8c0499dc1fa81864919b
Ben Kite <bakite@ku.edu>
2017-07-01 07:55:12 -0500

    added script to pull information from baseball reference on pitcher handedness

---
+++ Python/handedness.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+## Ben Kite
+
+import pandas, numpy
+import requests, bs4
+import re, os
+
+## Pulls a single table from a url provided by the user.
+## The desired table should be specified by tableID.
+## This function is used in all functions that do more complicated pulls.
+def pullTable(url, tableID):
+    res = requests.get(url)
+    ## Work around comments
+    comm = re.compile("<!--|-->")
+    soup = bs4.BeautifulSoup(comm.sub("", res.text), 'lxml')
+    tables = soup.findAll('table', id = tableID)
+    data_rows = tables[0].findAll('tr')
+    data_header = tables[0].findAll('thead')
+    data_header = data_header[0].findAll("tr")
+    data_header = data_header[0].findAll("th")
+    game_data = [[td.getText() for td in data_rows[i].findAll(['th','td'])]
+        for i in range(len(data_rows))
+        ]
+    data = pandas.DataFrame(game_data)
+    header = []
+    for i in range(len(data.columns)):
+        header.append(data_header[i].getText())
+    data.columns = header
+    data = data.loc[data[header[0]] != header[0]]
+    data = data.reset_index(drop = True)
+    return(data)
+
+## This pulls information about which hand a pitcher throws with.  I
+## made this solely to allow pitcher handedness to be used as a
+## variable in models.
+def pullPitcherData (team, year):
+    url = "http://www.baseball-reference.com/teams/" + team + "/" + str(year) + ".shtml"
+    data = pullTable(url, "team_pitching")
+    data = data[data.Name.notnull()]
+    data = data[data.Rk.notnull()]
+    data = data[data.G != "162"]
+    data = data.reset_index(drop = True)
+    data["Team"] = team
+    data["Year"] = year
+    data["LeftHanded"] = data["Name"].str.contains("\\*")
+    names = data.columns
+    for c in range(0, len(names)):
+        replacement = []
+        if type (data.loc[0][c]) == str:
+            k = names[c]
+            for i in range(0, len(data[k])):
+                p = data.loc[i][c]
+                xx = re.sub("[#@&*^%$!]", "", p)
+                xx = xx.replace("\xa0", "_")
+                xx = xx.replace(" ", "_")
+                replacement.append(xx)
+            data[k] = replacement
+    data = data[["Name", "LeftHanded", "Team", "Year"]]
+    return(data)
+
+teams = ['ATL', 'ARI', 'BAL', 'BOS', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', 'DET',
+         'KCR', 'HOU', 'LAA', 'LAD', 'MIA', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK',
+         'PHI', 'PIT', 'SDP', 'SEA', 'SFG', 'STL', 'TBR', 'TEX', 'TOR', 'WSN']
+
+dat = dict()
+for t in teams:
+    dat[t] = pullPitcherData(t, 2017)
+
+handedness = pandas.concat(dat)
+
+handedness.to_csv("handedness.csv")
+
+    
